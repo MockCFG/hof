1a5e9c8d7edb82c24155f72d4d6612959d00e208
Soundarya Tekkalakota <soundarya.tekkalakota1@gmail.com>
2017-07-09 18:48:08 +0530

    added user home
    navigates to user home on login button press. please check

---
+++ userhome.php
@@ -0,0 +1,225 @@
+<html>
+<head>
+
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Theme Template for Bootstrap</title>
+      <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="../../dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="theme.css" rel="stylesheet">
+    <link href="userstyles.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.1/jquery.min.js"></script>
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+    <script type="text/javascript" src="usershome.js"></script>
+</head>
+
+      <header class="clearfix feed-header" data-role="banner">
+        <div class="compWrapper">
+            <div class="compInnerWrapper">
+                <div class="compContainer" id="cycler1">
+                    <div class="compPhoto compPhoto1">
+                        <a href="#">
+                            <div class="Image iWithTransition">
+                                <img  src="http://placehold.it/225x225/888/fff&text=photo1"/>
+                                <b class="compPhotoShadow"></b>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="compPhoto compPhoto2">
+                        <a href="#">
+                            <div class="Image iLoaded iWithTransition">
+                                <img src="http://placehold.it/225x225/545454/fff&text=photo2"/>
+                                <b class="compPhotoShadow"></b>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="compPhoto compPhoto3">
+                        <a href="#" class="">
+                            <div class="Image iWithTransition">
+                                <img src="http://placehold.it/425x425/157B86/fff&text=photo3"/>
+                                <b class="compPhotoShadow"></b>
+                            </div>
+                        </a>
+                        
+
+                    </div>
+                    <div class="compPhoto compPhoto4">
+                        <a href="#">
+                            <div class="Image iWithTransition">
+                                <img src="http://placehold.it/250x250/222/fff&text=photo4"/>
+                                <b class="compPhotoShadow"></b>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="compPhoto compPhoto5">
+                        <a href="#">
+                            <div class="Image iWithTransition">
+                                <img src="http://placehold.it/220x220/444/fff&text=photo5"/>
+                                <b class="compPhotoShadow"></b>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="compPhoto compPhoto6">
+                        <a href="#">
+                            <div class="Image iWithTransition">
+                                <img src="http://placehold.it/230x230/333/fff&text=photo6"/>
+                                <b class="compPhotoShadow"></b>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="compPhoto compPhoto7">
+                        <a href="#">
+                            <div class="Image iWithTransition">
+                                <img src="http://placehold.it/250x250/555/fff&text=photo7"/>
+                                <b class="compPhotoShadow"></b>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">HallOfFame</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+           
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="profile-user">
+      <div class="row-fluid">
+    <div class="span6">
+      <div class="pull-left" style="margin-right:15px">
+          <img src="http://www.gravatar.com/avatar/f6112e781842d6a2b4636b35451401ff?s=80&d=mm&r=g" class="img-polaroid"/>
+      </div>
+
+      <h2>jonniespratley</h2>
+      <p>
+        <strong>Jonnie Spratley</strong> <a href="#">http://jonniespratley.me</a>
+      </p>
+    </div>
+    <div class="span6">
+      <ul class="inline user-counts-list">
+        <li>Photos <span class="count">100</span></li>
+        <li>Followers <span class="count">50</span></li>
+        <li>Following <span class="count">130</span></li>
+      </ul>
+    </div>
+  </div>
+      
+      
+  </div>
+
+
+
+
+<div class="feed-photos">
+ 
+  <div class="row-fluid">
+    <div class="span12">
+      <ul class="thumbnails user-media-thumbnails">
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/150x150"/>
+            <div class="meta">
+              <i class="icon-heart"></i> 3
+            
+              <i class="icon-comment"></i> 0
+            </div>
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/150x150"/>
+            <div class="meta">
+              <i class="icon-heart"></i> 3
+            
+              <i class="icon-comment"></i> 0
+            </div>
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/150x150"/>
+            <div class="meta">
+              <i class="icon-heart"></i> 3
+            
+              <i class="icon-comment"></i> 0
+            </div>
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/150x150"/>
+            <div class="meta">
+              <i class="icon-heart"></i> 3
+            
+              <i class="icon-comment"></i> 0
+            </div>
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/150x150"/>
+            <div class="meta">
+              <i class="icon-heart"></i> 3
+            
+              <i class="icon-comment"></i> 0
+            </div>
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/150x150"/>
+            <div class="meta">
+              <i class="icon-heart"></i> 3
+            
+              <i class="icon-comment"></i> 0
+            </div>
+          </a>
+        </li>
+      
+      </ul>
+    </div>
+  </div>
+</div>
+</body>
+</html>
